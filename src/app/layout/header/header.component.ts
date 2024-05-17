b4e8cfa31a979b79e2f4e4b92a241882ce88c3e3
Ranko1996 <koturic96@gmail.com>
2024-05-17 14:11:32 +0200

    Small rework

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService, private router: Router) {}
 
   logout() {
     this.authService.logout();
+  }
+
+  navigateTo(route: string) {
+    this.router.navigate([route]);
   }
 }
