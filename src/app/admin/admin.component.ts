28d76a1cff5fb6cd7e4f45350a187b3be226b975
Ranko1996 <koturic96@gmail.com>
2024-05-17 13:58:52 +0200

    Admin set and user deletion complete

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminService } from '../services/admin.service';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../layout/header/header.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent, ButtonModule],
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
@@ -14,7 +16,9 @@ export class AdminComponent implements OnInit {
 
   constructor(private adminService: AdminService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchUsers();
+  }
 
   fetchUsers() {
     this.adminService.getUsers().subscribe({
@@ -24,6 +28,33 @@ export class AdminComponent implements OnInit {
       },
       error: err => {
         console.error('Error fetching users:', err);
+      }
+    });
+  }
+
+  deleteUser(userId: string) {
+    console.log('Deleting user with id:', userId); // Logirajte userId
+    this.adminService.deleteUser(userId).subscribe({
+      next: () => {
+        console.log(`User ${userId} deleted`);
+        this.fetchUsers(); // Refresh the user list
+      },
+      error: err => {
+        console.error('Error deleting user:', err);
+      }
+    });
+  }
+  
+
+  setAsAdmin(userId: string) {
+    console.log(`SET AD SDMIN ${userId}`)
+    this.adminService.setAsAdmin(userId).subscribe({
+      next: () => {
+        console.log(`User ${userId} set as admin`);
+        this.fetchUsers(); // Refresh the user list
+      },
+      error: err => {
+        console.error('Error setting user as admin:', err);
       }
     });
   }
--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -18,6 +18,7 @@ export class AdminService {
             const headers = new HttpHeaders().set('Authorization', `Bearer ${idToken}`);
             this.http.get('http://localhost:3000/admin/getUsers', { headers, withCredentials: true }).subscribe({
               next: (data) => {
+                console.log(data);
                 observer.next(data);
                 observer.complete();
               },
@@ -32,4 +33,57 @@ export class AdminService {
       });
     });
   }
+
+  setAsAdmin(userId: string): Observable<any> {
+    return new Observable(observer => {
+      onIdTokenChanged(this.auth, user => {
+        if (user) {
+          user.getIdToken().then(idToken => {
+            const headers = new HttpHeaders().set('Authorization', `Bearer ${idToken}`);
+            this.http.post(`http://localhost:3000/admin/setAdmin/${userId}`, {}, { headers, withCredentials: true }).subscribe({
+              next: (data) => {
+                console.log('Korisnik postavljen za admina:', data); // Logirajte odgovor
+                observer.next(data);
+                observer.complete();
+              },
+              error: (err) => {
+                observer.error(err);
+              }
+            });
+          });
+        } else {
+          observer.error('Korisnik nije prijavljen');
+        }
+      });
+    });
+  }
+  
+  
+  
+  deleteUser(userId: string): Observable<any> {
+    return new Observable(observer => {
+      onIdTokenChanged(this.auth, user => {
+        if (user) {
+          user.getIdToken().then(idToken => {
+            const headers = new HttpHeaders().set('Authorization', `Bearer ${idToken}`);
+            this.http.delete(`http://localhost:3000/admin/deleteUser/${userId}`, { headers, withCredentials: true, responseType: 'text' }).subscribe({
+              next: (data) => {
+                console.log('Korisnik obrisan:', data); // Logirajte odgovor
+                observer.next(data);
+                observer.complete();
+              },
+              error: (err) => {
+                observer.error(err);
+              }
+            });
+          });
+        } else {
+          observer.error('Korisnik nije prijavljen');
+        }
+      });
+    });
+  }
+  
+  
+  
 }
